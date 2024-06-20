e360d9cf1e03ca55122c9e3754e533751366e433
Oleksii Kyslytsyn <kislitsyn.aleksey@gmail.com>
2024-06-20 21:06:20 +0300

    make build

---
+++ index.c228e09a.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var n=e.target.closest(".list-item__link");n&&(t.src=n.href)});
+//# sourceMappingURL=index.c228e09a.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const galleryList = document.getElementById("thumbs");
+const largeImg = document.getElementById("largeImg");
+galleryList.addEventListener("click", (action)=>{
+    action.preventDefault();
+    const img = action.target.closest(".list-item__link");
+    if (!img) return;
+    largeImg.src = img.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
